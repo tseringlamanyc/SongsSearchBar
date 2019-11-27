be9189d3034086b5451d3dbd21c15777dc736429
Tsering Lama <tseringlama@pursuit.org>
2019-11-27 11:12:34 -0500

    set up the UI for the lab

--- SongsTableViewSearchBar/ViewController.swift
+++ SongsTableViewSearchBar/ViewController.swift
@@ -9,19 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+        tableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "songCell", for: indexPath)
+        return cell
+    }
 }
 
